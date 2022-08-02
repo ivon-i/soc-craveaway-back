f1c7e8536b8ab00e9aaea6f01f0215df50e78e9b
ivonivanova <ivon.b.ivanova@gmail.com>
2022-08-02 14:18:03 +0100

    table popped

--- db/scripts/recipes/create.js
+++ db/scripts/recipes/create.js
@@ -2,7 +2,7 @@ import { query } from "../../index.js";
 
 async function createRecipesTable() {
   const res = await query(
-    `CREATE TABLE IF NOT EXISTS recipes (recipe_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, title TEXT, author TEXT, description TEXT, time TEXT, cost TEXT, ingredients TEXT, image TEXT, serves INT, rating INT, rating_entries INT);`
+    `CREATE TABLE IF NOT EXISTS recipes (recipe_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, title TEXT, author TEXT, description TEXT, time TEXT, cost TEXT, nutrition TEXT, ingredients TEXT, image TEXT, serves INT, rating INT, rating_entries INT);`
   );
   console.log(res.command, "recipes table created");
 }
--- db/scripts/recipes/populate.js
+++ db/scripts/recipes/populate.js
@@ -4,16 +4,20 @@ import { data } from "../../../libs/recipeData.js";
 async function populateRecipesTable() {
   for (let i = 0; i < data.length; i++) {
     const res = await query(
-      `INSERT INTO tickets (name, roomnumber, message, keywords, status) VALUES ($1, $2, $3, $4, $5);`,
-      [
-        tickets[i].name,
-        tickets[i].roomnumber,
-        tickets[i].message,
-        tickets[i].keywords,
-        tickets[i].status,
-      ]
+      `INSERT INTO recipes (title, author, description, time, cost, nutrition, ingredients, image, serves, rating, rating_entries) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *;`,
+      [data[i].title,
+      data[i].author, 
+      data[i].description, 
+      data[i].time, 
+      data[i].cost, 
+      data[i].nutrition, 
+      data[i].ingredients, 
+      data[i].image, 
+      data[i].serves, 
+      data[i].rating, 
+      data[i].rating_entries]
     );
-    console.log(`Added to DB: ${res.rows[0].ticket}`);
+    console.log(`Added to DB: ${res.rows[0].title}`);
   }
 }
 
