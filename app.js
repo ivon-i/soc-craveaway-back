afff934cbad9efd4439de2478d02e704cb28bc96
ivonivanova <ivon.b.ivanova@gmail.com>
2022-08-04 14:46:51 +0100

    cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from "express";
 // import bodyParser from "body-parser";
 import recRouter from "./routes/recipes.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
