b01181316cf13e9db2c2da551d291a2f85c53452
theleonmo <le.mo@live.co.uk>
2022-08-09 14:50:07 +0100

    recipe scripts updated

--- db/scripts/recipes/populate.js
+++ db/scripts/recipes/populate.js
@@ -1,23 +1,28 @@
 import { query } from "../../index.js";
 import { data } from "../../../libs/recipeData.js";
 
+    
 async function populateRecipesTable() {
   for (let i = 0; i < data.length; i++) {
     const res = await query(
-      `INSERT INTO recipes (title, author, description, time, cost, nutrition, ingredients, image, serves, rating, rating_entries) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *;`,
-      [data[i].title,
-      data[i].author, 
-      data[i].description, 
-      data[i].time, 
-      data[i].cost, 
-      data[i].nutrition, 
-      data[i].ingredients, 
-      data[i].image, 
-      data[i].serves, 
-      data[i].rating, 
-      data[i].rating_entries]
+      `INSERT INTO recipes (title, author, description, time, cost, nutrition, ingredients, image, serves, rating, rating_entries, cloudinary_id, image_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *;`,
+      [
+        data[i].title,
+        data[i].author,
+        data[i].description,
+        data[i].time,
+        data[i].cost,
+        data[i].nutrition,
+        data[i].ingredients,
+        data[i].image,
+        data[i].serves,
+        data[i].rating,
+        data[i].rating_entries,
+        data[i].cloudinary_id,
+        data[i].image_url,
+      ]
     );
-    console.log(`Added to DB: ${res.rows[0].title}`);
+    console.log(`Added to DB`);
   }
 }
 
