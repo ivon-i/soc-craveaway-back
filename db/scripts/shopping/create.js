bd109cdf0792dd3f9a9e2a614cdb773c142ed445
ivonivanova <ivon.b.ivanova@gmail.com>
2022-08-12 10:50:19 +0100

    comment to redeploy

--- db/scripts/shopping/create.js
+++ db/scripts/shopping/create.js
@@ -1,5 +1,6 @@
 import { query } from '../../index.js';
 
+//creates a table which will consist of all ingredients added by users (to the respective shopping list)
 async function createShopTable() {
   const res = await query(
     `CREATE TABLE IF NOT EXISTS shopping (item_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, username TEXT, item TEXT);`
