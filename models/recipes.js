cb41c09398806031a649afd06eec8b662930e09e
ivonivanova <ivon.b.ivanova@gmail.com>
2022-08-16 14:35:51 +0100

    dropped and redeployed

--- models/recipes.js
+++ models/recipes.js
@@ -69,7 +69,7 @@ export async function updateRating(id, updated) {
   );
   return data.rows;
 }
-//currently calculates average of all ratings in db
+//currently calculates average of all ratings in database
 export async function calculateAvg() {
   const data = await query(`SELECT AVG(ALL rating) FROM recipes`);
   return data.rows;
